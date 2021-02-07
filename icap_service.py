716cb819657f733515a7301c4d502a3872c1dea8
Jesus Rodriguez Fonteboa <hiperesfera@gmail.com>
2021-02-07 20:55:18 +0000

    Update icap_service.py

--- icap_service.py
+++ icap_service.py
@@ -4,14 +4,12 @@ import logging
 
 from pyicap import *
 
-
 logging.basicConfig(
-    filename='/var/log/icap.log',
+    filename='/var/log/icap_service.log',
     level=logging.NOTSET,
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
-
 
 
 class ThreadingSimpleServer(SocketServer.ThreadingMixIn, ICAPServer):
@@ -22,14 +20,23 @@ class ICAPHandler(BaseICAPRequestHandler):
     def echo_OPTIONS(self):
         self.set_icap_response(200)
         self.set_icap_header('Methods', 'REQMOD')
-        self.set_icap_header('Preview', '0')
+        #self.set_icap_header('Preview', '0')
         self.send_headers(False)
 
     def echo_REQMOD(self):
-        logging.info('Evaluating the request')
+        # HTTP GET Headers
         if self.enc_req[0] != "POST":
+            self.set_icap_response(200)
+            self.set_enc_request(' '.join(self.enc_req))
+            for h in self.enc_req_headers:
+                for v in self.enc_req_headers[h]:
+                    logging.info('HTTP Headers %s : %s', h, v)
+                    self.set_enc_header(h, v)
+            self.send_headers(False)
             self.no_adaptation_required()
+
         else:
+            # HTTP POST Headers
             self.set_icap_response(200)
             if not self.has_body:
                 self.set_enc_request(' '.join(self.enc_req))
@@ -38,8 +45,10 @@ class ICAPHandler(BaseICAPRequestHandler):
                         logging.info('HTTP Headers %s : %s', h, v)
                         self.set_enc_header(h, v)
                 self.send_headers(False)
+                self.no_adaptation_required()
                 return
 
+            # HTTP POST Body
             buff = ''
             while True:
                 chunk = self.read_chunk()
@@ -47,20 +56,12 @@ class ICAPHandler(BaseICAPRequestHandler):
                     break
                 buff += chunk
 
-            buff += "&TestKey=TestValue"
             logging.info("HTTP POST %s", buff)
-            self.set_enc_request(' '.join(self.enc_req))
-            for h in self.enc_req_headers:
-                for v in self.enc_req_headers[h]:
-                    if h.lower() == 'content-length':
-                        v = str(len(buff))
-                    self.set_enc_header(h, v)
 
-            self.send_headers(True)
-            self.send_chunk(buff)
+            self.send_headers(False)
+            self.no_adaptation_required()
 
-
-server = ThreadingSimpleServer(('', 1344), ICAPHandler)
+server = ThreadingSimpleServer(('127.0.0.1', 1344), ICAPHandler)
 try:
     while 1:
         server.handle_request()
